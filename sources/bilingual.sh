d927b08de4116f017c574f0fac6abc8d1fda7da2
Nick Nicholas <opoudjis@gmail.com>
2020-09-26 00:17:01 +1000

    update

--- sources/bilingual.sh
+++ sources/bilingual.sh
@@ -1,1 +1,3 @@
+asciidoctor -b bipm -r metanorma-bipm si-brochure-en.adoc
+asciidoctor -b bipm -r metanorma-bipm si-brochure-fr.adoc
 bundle exec metanorma collection si-brochure-bilingual.yml -x xml,html,presentation -w bilingual-brochure -c collection_cover.html
