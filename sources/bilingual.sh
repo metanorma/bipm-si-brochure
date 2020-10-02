9aa1441c9589bba05d76fc210cfba69ca65052a9
Nick Nicholas <opoudjis@gmail.com>
2020-10-02 21:58:22 +1000

    si-aspect: https://github.com/metanorma/metanorma-bipm/issues/14

--- sources/bilingual.sh
+++ sources/bilingual.sh
@@ -1,3 +1,5 @@
+asciidoctor -b bipm -r metanorma-bipm sib-a4-en.adoc
+asciidoctor -b bipm -r metanorma-bipm sib-a4-fr.adoc
 asciidoctor -b bipm -r metanorma-bipm si-brochure-en.adoc
 asciidoctor -b bipm -r metanorma-bipm si-brochure-fr.adoc
 bundle exec metanorma collection si-brochure-bilingual.yml -x xml,html,presentation -w bilingual-brochure -c collection_cover.html
