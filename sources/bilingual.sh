d3b97299fccefa8d96448b845e33296e657b8874
Nick Nicholas <opoudjis@gmail.com>
2020-10-14 21:02:29 +1100

    compilation script

--- sources/bilingual.sh
+++ sources/bilingual.sh
@@ -1,13 +1,13 @@
-asciidoctor -b bipm -r metanorma-bipm mep-ampere-definition-en.adoc
-asciidoctor -b bipm -r metanorma-bipm mep-candela-rapport.adoc
-asciidoctor -b bipm -r metanorma-bipm mep-candela-si-app2.adoc
-asciidoctor -b bipm -r metanorma-bipm mep-kelvin-definition-en.adoc
-asciidoctor -b bipm -r metanorma-bipm mep-kilogram-definition-en.adoc
-asciidoctor -b bipm -r metanorma-bipm mep-metre-definition-en.adoc
-asciidoctor -b bipm -r metanorma-bipm mep-mole-definition-en.adoc
-asciidoctor -b bipm -r metanorma-bipm mep-second-definition-en.adoc
-asciidoctor -b bipm -r metanorma-bipm sib-a4-en.adoc
-asciidoctor -b bipm -r metanorma-bipm sib-a4-fr.adoc
-asciidoctor -b bipm -r metanorma-bipm si-brochure-en.adoc
-asciidoctor -b bipm -r metanorma-bipm si-brochure-fr.adoc
+bundle exec metanorma -t bipm mep-ampere-definition-en.adoc
+bundle exec metanorma -t bipm metanorma-bipm mep-candela-rapport.adoc
+bundle exec metanorma -t bipm mep-candela-si-app2.adoc
+bundle exec metanorma -t bipm mep-kelvin-definition-en.adoc
+bundle exec metanorma -t bipm mep-kilogram-definition-en.adoc
+bundle exec metanorma -t bipm mep-metre-definition-en.adoc
+bundle exec metanorma -t bipm mep-mole-definition-en.adoc
+bundle exec metanorma -t bipm mep-second-definition-en.adoc
+bundle exec metanorma -t bipm sib-a4-en.adoc
+bundle exec metanorma -t bipm sib-a4-fr.adoc
+bundle exec metanorma -t bipm si-brochure-en.adoc
+bundle exec metanorma -t bipm si-brochure-fr.adoc
 bundle exec metanorma collection si-brochure-bilingual.yml -x xml,html,presentation,pdf -w bilingual-brochure -c collection_cover.html
