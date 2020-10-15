dc53a679dedff1b20637af170f1b17f57b2cde67
Ronald Tse <ronald.tse@ribose.com>
2020-10-16 01:18:57 +0800

    Update build flow

--- sources/bilingual.sh
+++ sources/bilingual.sh
@@ -1,13 +1,13 @@
-bundle exec metanorma -t bipm mep-ampere-definition-en.adoc
-bundle exec metanorma -t bipm metanorma-bipm mep-candela-rapport.adoc
-bundle exec metanorma -t bipm mep-candela-si-app2.adoc
-bundle exec metanorma -t bipm mep-kelvin-definition-en.adoc
-bundle exec metanorma -t bipm mep-kilogram-definition-en.adoc
-bundle exec metanorma -t bipm mep-metre-definition-en.adoc
-bundle exec metanorma -t bipm mep-mole-definition-en.adoc
-bundle exec metanorma -t bipm mep-second-definition-en.adoc
-bundle exec metanorma -t bipm sib-a4-en.adoc
-bundle exec metanorma -t bipm sib-a4-fr.adoc
-bundle exec metanorma -t bipm si-brochure-en.adoc
-bundle exec metanorma -t bipm si-brochure-fr.adoc
+bundle exec metanorma mep-ampere-definition-en.adoc
+bundle exec metanorma metanorma-bipm mep-candela-rapport.adoc
+bundle exec metanorma mep-candela-si-app2.adoc
+bundle exec metanorma mep-kelvin-definition-en.adoc
+bundle exec metanorma mep-kilogram-definition-en.adoc
+bundle exec metanorma mep-metre-definition-en.adoc
+bundle exec metanorma mep-mole-definition-en.adoc
+bundle exec metanorma mep-second-definition-en.adoc
+bundle exec metanorma sib-a4-en.adoc
+bundle exec metanorma sib-a4-fr.adoc
+bundle exec metanorma si-brochure-en.adoc
+bundle exec metanorma si-brochure-fr.adoc
 bundle exec metanorma collection si-brochure-bilingual.yml -x xml,html,presentation,pdf -w bilingual-brochure -c collection_cover.html
