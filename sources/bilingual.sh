14f1d21c3d34b3f3abe5ccfd06ee41c1011dece1
Nick Nicholas <opoudjis@gmail.com>
2020-11-09 21:17:11 +1100

    script

--- sources/bilingual.sh
+++ sources/bilingual.sh
@@ -1,3 +1,4 @@
+bundle exec metanorma 2020-02/document.adoc
 bundle exec metanorma guide-ccem-ampere-revised-si-en.adoc	
 bundle exec metanorma guide-ccl-GD-MeP-1-en.adoc		
 bundle exec metanorma guide-ccl-GD-MeP-2-en.adoc		
@@ -5,7 +6,7 @@ bundle exec metanorma guide-ccl-GD-MeP-3-en.adoc
 bundle exec metanorma guide-ccm-kilogram-calibration-en.adoc	
 bundle exec metanorma guide-ccm-kilogram-dissemination-en.adoc
 bundle exec metanorma mep-ampere-definition-en.adoc
-bundle exec metanorma metanorma-bipm mep-candela-rapport.adoc
+bundle exec metanorma mep-candela-rapport.adoc
 bundle exec metanorma mep-candela-si-app2.adoc
 bundle exec metanorma mep-kelvin-definition-en.adoc
 bundle exec metanorma mep-kilogram-definition-en.adoc
