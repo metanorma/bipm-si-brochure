069b1fe27f3c71f25a56c7ae05cac3b4fe90e2f0
Ronald Tse <ronald.tse@ribose.com>
2021-02-06 11:14:55 +0800

    Remove bilingual.sh

--- sources/bilingual.sh
+++
@@ -1,2 +0,0 @@
-bundle exec metanorma site generate . -o published -c collection.yml
-bundle exec metanorma collection si-brochure-bilingual.yml -x xml,html,presentation,pdf -w bilingual-brochure -c collection_cover.html
